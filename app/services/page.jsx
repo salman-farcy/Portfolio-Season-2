19aa4f901dcbde15d2c2440b6c0c75555d828910
slaman-farcy <salmanfarcy253@gmail.com>
2024-07-15 14:50:59 +0600

    Navbar front-end design and some functionality is complete

---
+++ app/services/page.jsx
@@ -0,0 +1,11 @@
+
+
+const Services = () => {
+  return (
+    <div>
+      <div className="">services page</div>
+    </div>
+  )
+}
+
+export default Services;
---
+++ components/Stairs.jsx
@@ -0,0 +1,47 @@
+"use client";
+import { animate, motion } from "framer-motion";
+
+//variants
+const stairAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
+//calculate the reverse index fot staggred dalay
+const reverseIndex = (index) => {
+  const totaLStapes = 6; // number of stepes
+  return totaLStapes - index - 1;
+};
+
+const Stairs = () => {
+  return (
+    <>
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-accent relative"
+          />
+        );
+      })}
+    </>
+  );
+};
+
+export default Stairs;
