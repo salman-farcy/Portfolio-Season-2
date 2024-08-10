22d6919ed0d51ad69906846b1a45c310aa3206c3
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-10 21:46:22 +0600

    contact page work start

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+"use client";
+
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Select, SelectContent, SelectGroup, SelectiItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {FaPhoneAlt, FaEnvelope, FaMapMarkerAlt} from "react-icons/fa";
+
+
+const info = [
+  {
+    icon: <FaPhoneAlt/>,
+    title: "Phone",
+    description: "01602695577",
+  },
+  {
+    icon: <FaEnvelope/>,
+    title: "Email",
+    description: "salmanfarcy253@gmail.com",
+  },
+  {
+    icon: <FaMapMarkerAlt/>,
+    title: "Address",
+    description: "Basila, Mohammadpur, Dhaka 1207, Bangladesh",
+  },
+];
+
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <div>
+    <motion.section>
       contact page
-    </div>
+    </motion.section>
   )
 }
 
