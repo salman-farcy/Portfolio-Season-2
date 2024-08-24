a5fa82cca47d77462a2a7bad3106a04cbbf90755
slaman-farcy <salmanfarcy253@gmail.com>
2024-08-24 16:29:09 +0600

    Contact section UI design complete

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -8,6 +8,7 @@ import {
   SelectContent,
   SelectGroup,
   SelectiItem,
+  SelectItem,
   SelectLabel,
   SelectTrigger,
   SelectValue,
@@ -18,7 +19,7 @@ const info = [
   {
     icon: <FaPhoneAlt />,
     title: "Phone",
-    description: "01602695577",
+    description: "+8801602695577",
   },
   {
     icon: <FaEnvelope />,
@@ -29,7 +30,7 @@ const info = [
     icon: <FaMapMarkerAlt />,
     title: "Address",
     description: "Basila, Mohammadpur, Dhaka 1207, Bangladesh",
-  },
+  }
 ];
 
 import { motion } from "framer-motion";
@@ -50,16 +51,68 @@ const Contact = () => {
           <div className="xl:-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let's work together</h3>
-              <p className="text-white/60">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae, accusamus.</p>
+              <p className="text-white/60">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vitae,
+                accusamus.
+              </p>
+
+              {/* input */}
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <Input type="firstname" placeholder="First Name" />
+                <Input type="lastname" placeholder="Last Name" />
+                <Input type="email" placeholder="Email address" />
+                <Input type="phone" placeholder="Phone Number" />
+              </div>
+
+              {/* select */}
+              <Select>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select a Service" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select a Service</SelectLabel>
+                    <SelectItem value="est">Web Development</SelectItem>
+                    <SelectItem value="cst">UI/UX Design</SelectItem>
+                    <SelectItem value="mst">Logo Design</SelectItem>
+                    <SelectItem value="nst">SEO</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              {/* textarea */}
+              <Textarea
+                className="h-[200px] "
+                placeholder="Type Your Message Here."
+              />
+
+              {/* btn */}
+              <Button size="md" className="max-w-40">
+                Send Message
+              </Button>
             </form>
           </div>
 
           {/* info */}
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-           <Input type="firstname" placeholder="First Name"/>
-           <Input type="lastname" placeholder="Last Name"/>
-           <Input type="email" placeholder="Email address"/>
-           <Input type="phone" placeholder="Phone Number"/>
+          <div className="flex-1 flex items-center xl:justify-end order-1">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, i) => {
+                return (
+                  <li
+                    className="flex items-center gap-6 bg-[#27272c] p-3 rounded-md"
+                    key={i}
+                  >
+                    <div>
+                      <div className="w-14 h-14 xl:w-16 xl:h-16 bg-[#1e1e22] text-accent rounded-md flex items-center justify-center text-xl">{item.icon}</div>
+                      </div>
+                    <div>
+                      <p className="text-white/60 ">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
